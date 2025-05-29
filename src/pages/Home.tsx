1ac727fbe43a7675ee18ed3b72756cf4744a4375
Michael <mikegriggs929@gmail.com>
2025-05-29 19:15:30 -0400

    Add files via upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className='p-6 text-2xl font-bold text-center'>
-      <h1>Welcome to The Father's Alliance</h1>
-      <p className='mt-4 text-lg'>We are rising.</p>
+    <div className='min-h-screen flex flex-col items-center justify-center bg-black text-white text-3xl font-bold'>
+      <h1>Resurgence 2.0 is LIVE</h1>
+      <p className='mt-4 text-xl'>If you're seeing this, the site is working.</p>
     </div>
   );
 }
