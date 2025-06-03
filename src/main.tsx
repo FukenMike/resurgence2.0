33a000db8ca8f224cca62ee29f041c279ed31c23
Michael <mikegriggs929@gmail.com>
2025-06-03 03:04:12 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 
-import Home from './pages/Home';
-import WhoWeAre from './pages/WhoWeAre';
-import Mission from './pages/Mission';
-import Vision from './pages/Vision';
-import FounderStory from './pages/FounderStory';
-import Donate from './pages/Donate';
-import BuriedByTheSystem from './pages/BuriedByTheSystem';
-import Publications from './pages/Publications';
-import WallOfTruth from './pages/WallOfTruth';
-import ApolloAlliance from './pages/ApolloAlliance';
-import Contact from './pages/Contact';
+import Home from './pages/home';
+import WhoWeAre from './pages/who-we-are';
+import Mission from './pages/mission';
+import Vision from './pages/vision';
+import FounderStory from './pages/founder-story';
+import Support from './pages/support';
+import BuriedByTheSystem from './pages/buried-by-the-system';
+import TheVault from './pages/the-vault';
+import WallOfTruth from './pages/wall-of-truth';
+import EducationReform from './pages/education-reform';
+import Connect from './pages/connect';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -24,12 +24,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='/mission' element={<Mission />} />
         <Route path='/vision' element={<Vision />} />
         <Route path='/founder-story' element={<FounderStory />} />
-        <Route path='/donate' element={<Donate />} />
+        <Route path='/support' element={<Support />} />
         <Route path='/buried-by-the-system' element={<BuriedByTheSystem />} />
-        <Route path='/publications' element={<Publications />} />
+        <Route path='/the-vault' element={<TheVault />} />
         <Route path='/wall-of-truth' element={<WallOfTruth />} />
-        <Route path='/apollo-alliance' element={<ApolloAlliance />} />
-        <Route path='/contact' element={<Contact />} />
+        <Route path='/education-reform' element={<EducationReform />} />
+        <Route path='/connect' element={<Connect />} />
       </Routes>
     </Router>
   </React.StrictMode>
