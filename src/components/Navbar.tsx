dce3e4edb0a72ec2c5110944f20dda3a7bc958ff
Michael <mikegriggs929@gmail.com>
2025-06-07 05:39:25 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,19 +45,18 @@ export default function Navbar() {
           className="lg:hidden flex items-center focus:outline-none"
         >
           <svg
-            className="w-6 h-6 fill-current text-amber-500"
+            className="w-6 h-6 text-amber-500"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             viewBox="0 0 24 24"
           >
             {isOpen ? (
-              <path
-                fillRule="evenodd"
-                d="M6 18L18 6M6 6l12 12"
-              />
+              <path d="M6 18L18 6M6 6l12 12" />
             ) : (
-              <path
-                fillRule="evenodd"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
+              <path d="M4 6h16M4 12h16M4 18h16" />
             )}
           </svg>
         </button>
