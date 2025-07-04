2c1d822f529058dc2167bd4a2d66e5441ab8163f
Michael <mikegriggs929@gmail.com>
2025-07-04 14:33:54 +0000

    vitedefault

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
+// vite.config.ts (bare minimum)
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: './',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
 });
