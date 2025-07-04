193502ec8ed4ba6d1f5a4192b13e7d9dde634b1d
Michael <mikegriggs929@gmail.com>
2025-07-04 14:51:10 +0000

    layoutfix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
     <div className="min-h-screen bg-steam-bg text-steam-text font-body">
       <Navbar />
-      <main className="p-4 lg:p-8 space-y-6">
+      <main className="pt-20 lg:pt-24 p-4 lg:p-8 space-y-6">
         <Outlet />
       </main>
       <Footer />
