0015798c388654bfc4618604795197222c151ac6
Michael <mikegriggs929@gmail.com>
2025-07-04 14:38:24 +0000

    layoutfix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 export default function Layout() {
   return (
     <div className="min-h-screen bg-steam-bg text-steam-text font-body">
       <Navbar />
       <main className="p-4 lg:p-8 space-y-6">
-        <Outlet /> {/* ← this renders the current route’s component */}
+        <Outlet />
       </main>
       <Footer />
     </div>
